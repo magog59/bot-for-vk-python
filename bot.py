147e4b2a1e4777b075cbc28c9e728361ae6c8d63
Nikita Ageev <65946687+magog59@users.noreply.github.com>
2021-02-27 20:36:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,5 @@
-# V.3
-# @link(https://vk.com/magog59) Основатель кода и его изобрёл.
-import os,sys
-sys.path.append('/home/m/magogsterb/tutorial-env/lib64/python3.4/site-packages/')
 from vk_api import VkApi
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
-import re
-import datetime
-import requests
-from bs4 import BeautifulSoup as BS
 
 vk_session = VkApi(token='токен')
 vk = vk_session.get_api()
